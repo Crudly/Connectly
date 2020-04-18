06eb45584859da06ad08923fe4117aa540a0d1c2
Larzs <wester@inbox.lv>
2020-04-18 22:27:14 +0300

    Initial commit

---
+++ src/Connectly.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Crudly\Connectly;
+
+class Connectly
+{
+    /**
+     * Create a new Eloquent query builder instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        dd('Yes');
+    }
+}
