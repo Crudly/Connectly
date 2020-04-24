c9c1a76ca6cd8cc45a7adc177cc98ddb1ab73357
Larzs <wester@inbox.lv>
2020-04-25 00:40:14 +0300

    Add Boilerplate for testing

---
+++ tests/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Crudly\Connectly\Tests;
+
+use Crudly\Connectly\Provider;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+abstract class TestCase extends Orchestra\Testbench\TestCase
+{
+    /**
+     * Load package service provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return Crudly\Connectly\Provider
+     */
+    protected function getPackageProviders($app)
+    {
+        return [Provider::class];
+    }
+}
