352752a86ef8eca78d498cf75d5c7467a2fa9200
Džuris <juris@glaive.pro>
2020-04-27 17:05:36 +0300

    Fix and expand connection creation test

--- tests/Integration/ConnectionCreationTest.php
+++ tests/Integration/ConnectionCreationTest.php
@@ -6,6 +6,9 @@ use Crudly\Connectly\Connectly;
 use Crudly\Connectly\Tests\TestCase;
 
 use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
+use Illuminate\Database\SqlServerConnection;
+use Illuminate\Database\SQLiteConnection;
 
 class ConnectionCreationTest extends TestCase
 {
@@ -23,6 +26,28 @@ class ConnectionCreationTest extends TestCase
 			'username' => 'connectly_user',
 			'password' => 'hunter2',
 		];
+
+		$this->pgsql = [
+			'driver' => 'pgsql',
+			'host' => '127.0.0.1',
+			'port' => '3306',
+			'database' => 'connectly_test_base',
+			'username' => 'connectly_user',
+			'password' => 'hunter2',
+		];
+
+		$this->sqlsrv = [
+			'driver' => 'sqlsrv',
+			'host' => '127.0.0.1',
+			'port' => '3306',
+			'database' => 'connectly_test_base',
+			'username' => 'connectly_user',
+			'password' => 'hunter2',
+		];
+
+		$this->sqlite = [
+			'driver' => 'sqlite',
+		];
 	}
 	
     /**
@@ -39,12 +64,90 @@ class ConnectionCreationTest extends TestCase
 		$connection = $connectly->connect();
 
         $this->assertInstanceOf(MySqlConnection::class, $connection);
-        $this->assertSame('mysql', $connectly->getDriverName());
-		$this->assertSame('connectly_test_base', $connectly->getDatabaseName());
+        $this->assertSame('mysql', $connection->getDriverName());
+		$this->assertSame('connectly_test_base', $connection->getDatabaseName());
 		
-		$config = $connectly->getConfig();
+		$config = $connection->getConfig();
 
 		foreach ($this->mysql as $key => $value)
+		{
+			$this->assertArrayHasKey($key, $config);
+			$this->assertSame($value, $config[$key]);
+		}
+    }
+	
+    /**
+     * Create a PostgreSQL connection from config.
+     *
+     * @return void
+     */
+    public function testPostgresFromConfig()
+    {
+		$connectly = new Connectly;
+
+		$connectly->config = $this->pgsql;
+
+		$connection = $connectly->connect();
+
+        $this->assertInstanceOf(PostgresConnection::class, $connection);
+        $this->assertSame('pgsql', $connection->getDriverName());
+		$this->assertSame('connectly_test_base', $connection->getDatabaseName());
+		
+		$config = $connection->getConfig();
+
+		foreach ($this->pgsql as $key => $value)
+		{
+			$this->assertArrayHasKey($key, $config);
+			$this->assertSame($value, $config[$key]);
+		}
+    }
+	
+    /**
+     * Create a MS SQL Server connection from config.
+     *
+     * @return void
+     */
+    public function testSqlServerFromConfig()
+    {
+		$connectly = new Connectly;
+
+		$connectly->config = $this->sqlsrv;
+
+		$connection = $connectly->connect();
+
+        $this->assertInstanceOf(SqlServerConnection::class, $connection);
+        $this->assertSame('sqlsrv', $connection->getDriverName());
+		$this->assertSame('connectly_test_base', $connection->getDatabaseName());
+		
+		$config = $connection->getConfig();
+
+		foreach ($this->sqlsrv as $key => $value)
+		{
+			$this->assertArrayHasKey($key, $config);
+			$this->assertSame($value, $config[$key]);
+		}
+    }
+	
+    /**
+     * Create a SQLite connection from config.
+     *
+     * @return void
+     */
+    public function testSQLiteFromConfig()
+    {
+		$connectly = new Connectly;
+
+		$connectly->config = $this->sqlite;
+
+		$connection = $connectly->connect();
+
+        $this->assertInstanceOf(SQLiteConnection::class, $connection);
+        $this->assertSame('sqlite', $connection->getDriverName());
+		$this->assertSame('connectly_test_base', $connection->getDatabaseName());
+		
+		$config = $connection->getConfig();
+
+		foreach ($this->sqlite as $key => $value)
 		{
 			$this->assertArrayHasKey($key, $config);
 			$this->assertSame($value, $config[$key]);
